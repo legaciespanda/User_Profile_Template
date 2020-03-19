2c6c3d71260778a731b37da0dec421ec716218e0
Ernest <youandinews@gmail.com>
2020-03-19 17:25:38 +0100

    Code written in the Main Activity

--- app/src/main/java/com/ernest/userprofiledesign/MainActivity.java
+++ app/src/main/java/com/ernest/userprofiledesign/MainActivity.java
@@ -2,12 +2,17 @@ package com.ernest.userprofiledesign;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import at.markushi.ui.CircleButton;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 /**
@@ -16,21 +21,33 @@ import butterknife.ButterKnife;
  * */
 
 public class MainActivity extends AppCompatActivity {
-    @BindView(R.id.StFirstLayout) RelativeLayout rl;
-    @BindView(R.id.StsecoundLayout) RelativeLayout r2;
-    @BindView(R.id.StthirdLayout) RelativeLayout r3;
-    @BindView(R.id.StFourthLayout) RelativeLayout r4;
-    @BindView(R.id.StFifthLayout) RelativeLayout r5;
-    @BindView(R.id.settings_button_rlayout) RelativeLayout r6;
-    @BindView(R.id.editprofile_button_rlayout) RelativeLayout r7;
-    @BindView(R.id.BoostButton) at.markushi.ui.CircleButton ff;
+    @BindView(R.id.StFirstLayout)
+    RelativeLayout rl;
+    @BindView(R.id.StsecoundLayout)
+    RelativeLayout r2;
+    @BindView(R.id.StthirdLayout)
+    RelativeLayout r3;
+    @BindView(R.id.StFourthLayout)
+    RelativeLayout r4;
+    @BindView(R.id.StFifthLayout)
+    RelativeLayout r5;
+    @BindView(R.id.settings_button_rlayout)
+    RelativeLayout r6;
+    @BindView(R.id.editprofile_button_rlayout)
+    RelativeLayout r7;
+    @BindView(R.id.BoostButton)
+    CircleButton ff;
+    @BindView(R.id.username)
+    TextView edt;
+    @BindView(R.id.Iconimage2)
+    CircleImageView profile_image;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        setContentView(R.layout.activity_main);
         initRelative();
         initViews();
     }
@@ -51,6 +68,9 @@ public class MainActivity extends AppCompatActivity {
                 //code for edit profile activity or fragment
             }
         });
+
+        //edt.setText("Obot Ernest");
+        //profile_image.setImageResource(R.drawable.obot);
 
 
     }
